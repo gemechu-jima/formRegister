c35a8748f683b4de05c589bf475364b97b31d3eb
gemechuj <gemechujimacs@gmail.com>
2024-09-17 15:12:44 +0300

    update

--- backend/src/controller/authController.js
+++ backend/src/controller/authController.js
@@ -1,6 +1,7 @@
 
 import { connection } from "../config/mysqlDB.js"
 import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
 
 
 function combineName(fname, lname){
@@ -9,10 +10,11 @@ function combineName(fname, lname){
    return firstName + lastName
 
 }
-const signUp=(req, res)=>{
+const signUp=async(req, res)=>{
     const {email, password, firstName, lastName}=req.body
+    const hashPassword=await bcrypt.hash(password, 10)
+    console.log(hashPassword)
    let userName= combineName( firstName, lastName)
-
     try {
        connection.query("SELECT username, email FROM auth WHERE email=?", [email], (error, data, fields) => {
         if (error) {
@@ -22,13 +24,13 @@ const signUp=(req, res)=>{
           return res.status(400).json({ msg: "Already exist, you cannot sign up again", error: true, success: false });
         }
           else{
-            connection.query("INSERT INTO auth (email, password, username) VALUES (  ?, ?, ?)", [email, password, userName],
+            connection.query("INSERT INTO auth (email, password, username) VALUES (  ?, ?, ?)", [email, hashPassword, userName],
                 (error, data, fields)=>{
                     if(error){ 
                         console.error(error)
                         return res.status(500).json({msg:error, error:true, success:false})
                     }else{
-                        return res.status(200).json({msg:"Successfully sign up",  error:false, success:true, data, token})
+                        return res.status(200).json({msg:"Successfully sign up",  error:false, success:true})
                     }
     
                        
@@ -42,22 +44,27 @@ const signUp=(req, res)=>{
 }
 
 
-const login=(req, res)=>{
+const login=async(req, res)=>{
     const {email, password}=req.body
-   
+  
     let token
     try {
-        connection.query("SELECT id, email FROM auth WHERE password=? AND email=?", [password, email], 
-            (error, data, fields)=>{
+         connection.query("SELECT id, email, password FROM auth WHERE  email=?", [email], 
+            async(error, data, fields)=>{
                 if(error){
-                    return res.status(500).json({msg:"Internal server error"})
+                    return res.status(500).json({msg:"Internal server error", error:true, success:false})
                 }else if(data.length===0){
-                return res.status(401).json({msg:"Invalid email and password"})
-
+                   return res.status(401).json({msg:"Invalid email and password", error:true, success:false})
                 }
-                token=jwt.sign({userI:data.id, email:data.email}, "12345", {expiresIn:"1d"})
-                return res.status(202).json({msg:"successfully login", data:token, userI:data.id, email:data.email})
-                
+                const user=data[0]
+                let hashPassword=await bcrypt.compare(password, user.password)
+                if(hashPassword){
+                    token=jwt.sign({userI:user.id, email:user.email}, "12345", {expiresIn:"1d"})
+                     return res.status(202).json({msg:"successfully login", 
+                         error:false, success:true, data:token, userI:data.id, email:data.email})
+                }else{
+                    return res.status(401).json({msg:"Your Password is incorrect", error:true, success:false})
+                }
             })
     } catch (error) {
        return  res.status(500).json({msg:"Internal server error",error})
